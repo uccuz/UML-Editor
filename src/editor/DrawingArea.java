41345e16822ab8e27bba99a55556e2eac153aa1c
uccu <uccu@uccu>
2021-04-05 22:25:56 +0800

    finish use case object graph

--- src/editor/DrawingArea.java
+++ src/editor/DrawingArea.java
@@ -1,8 +1,6 @@
 package editor;
 
 import java.awt.BasicStroke;
-import java.awt.Color;
-import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
@@ -13,8 +11,9 @@ import javax.swing.JPanel;
 import java.util.ArrayList;
 
 import modes.Mode;
-import shapes.ClassObject;
 import shapes.Shape;
+import shapes.ClassObject;
+import shapes.UseCaseObject;
 
 public class DrawingArea extends JPanel {
 
@@ -27,6 +26,7 @@ public class DrawingArea extends JPanel {
 		//this.setPreferredSize(new Dimension(100,100));
 		//this.setBackground(Color.green);
 		addShape(new ClassObject(100, 100));
+		addShape(new UseCaseObject(400, 100));
 	}
 	
 	public void addShape(Shape shape) {
@@ -36,7 +36,7 @@ public class DrawingArea extends JPanel {
 	public void paint(Graphics g) {
 		
 		Graphics2D g2d = (Graphics2D) g;
-		g2d.setStroke(new BasicStroke(5));
+		g2d.setStroke(new BasicStroke(4));
 		g2d.setFont(new Font("Monaco",Font.PLAIN,16));
 		
 		// Draw all shapes in arrayList 
--- src/shapes/UseCaseObject.java
+++ src/shapes/UseCaseObject.java
@@ -2,12 +2,12 @@ package shapes;
 
 import java.awt.Graphics;
 
-public class ClassObject extends BasicObject{
+public class UseCaseObject extends BasicObject{
 
 	static final int WIDTH = 120;
-	static final int HEIGHT = 150; 
+	static final int HEIGHT = 90; 
 	
-	public ClassObject(int x,int y) {
+	public UseCaseObject(int x,int y) {
 		// Set object size
 		this.width = WIDTH;
 		this.height = HEIGHT;		
@@ -17,18 +17,16 @@ public class ClassObject extends BasicObject{
 		this.maxX = x + this.width;
 		this.maxY = y + this.height;
 	}
-
+	
 	@Override
 	public void draw(Graphics g) {
-		
+
 		// Draw class object
-		g.drawRect(minX, minY, width, height);
-		g.drawLine(minX, minY + height/3 , maxX, minY + height/3);
-		g.drawLine(minX, minY + (height*2)/3 , maxX, minY + (height*2)/3);
+		g.drawOval(minX, minY, width, height);
 		
 		// Draw class name
 		int StringWidth = g.getFontMetrics(g.getFont()).stringWidth(name);
-		g.drawString(name, minX+(Math.abs(minX-maxX) - StringWidth)/2 ,minY + (Math.abs(minY-maxY)/6));
+		g.drawString(name, minX+(Math.abs(minX-maxX) - StringWidth)/2 ,minY + (Math.abs(minY-maxY)/2));
 		
 		
 	}
