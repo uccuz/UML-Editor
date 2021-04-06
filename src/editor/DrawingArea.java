d5a8273d94fb7fd3a4fe73e5c13ca8744c38bf3c
uccu <uccu@uccu>
2021-04-06 10:26:09 +0800

    change Tool Bar

--- src/editor/DrawingArea.java
+++ src/editor/DrawingArea.java
@@ -4,9 +4,11 @@ import java.awt.BasicStroke;
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 import javax.swing.JPanel;
-
 
 import java.util.ArrayList;
 
@@ -15,22 +17,40 @@ import shapes.Shape;
 import shapes.ClassObject;
 import shapes.UseCaseObject;
 
-public class DrawingArea extends JPanel {
+public class DrawingArea extends JPanel{
 
+	// singleton object
+	private static DrawingArea _drawingArea = null;
+	
 	private Mode mode = null;
 	
 	public ArrayList<Shape> shapes = new ArrayList<Shape>();
 	
 	DrawingArea() {
 		
-		//this.setPreferredSize(new Dimension(100,100));
-		//this.setBackground(Color.green);
+		// Add mouse listener
+		this.addMouseListener(new ClickListener());
+		
 		addShape(new ClassObject(100, 100));
 		addShape(new UseCaseObject(400, 100));
+		
+
+	}
+	
+	// Static method to create instance of Singleton class
+	public static DrawingArea getInstance() {
+		if (_drawingArea == null)
+			_drawingArea = new DrawingArea();
+  
+        return _drawingArea;
 	}
 	
 	public void addShape(Shape shape) {
 		shapes.add(shape);
+	}
+	
+	public void setMode(Mode mode) {
+		this.mode = mode;
 	}
 	
 	public void paint(Graphics g) {
@@ -45,4 +65,20 @@ public class DrawingArea extends JPanel {
 		});
 		
 	}
+
+	
+	private class ClickListener extends MouseAdapter{
+		
+		// Handle mouse event
+		public void mousePressed(MouseEvent e) {
+			System.out.println("pressed");
+			addShape(new ClassObject(e.getX(), e.getY()));
+			//repaint();
+		}
+		public void mouseReleased(MouseEvent e) {
+			System.out.println("release");
+		}
+	}
+	
+
 }
--- src/editor/ToolBar.java
+++ src/editor/ToolBar.java
@@ -2,15 +2,20 @@ package editor;
 
 import java.awt.Color;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JToolBar;
 
+import modes.*;
+
 public class ToolBar extends JToolBar{
 	
 	private final int btnNum = 6; 
 	
+	// Image source
 	ImageIcon selectIcon = new ImageIcon("img/select.png");
 	ImageIcon associationIcon = new ImageIcon("img/association.png");
 	ImageIcon generalizationIcon = new ImageIcon("img/generalization.png");
@@ -18,19 +23,25 @@ public class ToolBar extends JToolBar{
 	ImageIcon classIcon = new ImageIcon("img/class.png");
 	ImageIcon useCaseIcon = new ImageIcon("img/useCase.png");
 	
+	// Setting
+	DrawBtn holdBtn;
+	
 	ToolBar() {
+		
+		// Initialize setting
+		holdBtn = null;
 		
 		// ToolBar setting
 		this.setLayout(new GridLayout(btnNum,1));
 		this.setFloatable(false); // can't be moved
 		
 		// Create button
-		JButton selectBtn = new JButton(selectIcon);
-		JButton associationBtn = new JButton(associationIcon);
-		JButton generalizationBtn = new JButton(generalizationIcon);
-		JButton compositionBtn = new JButton(compositionIcon);
-		JButton classBtn = new JButton(classIcon);
-		JButton useCaseBtn = new JButton(useCaseIcon);
+		DrawBtn selectBtn = new DrawBtn(selectIcon, new Mode());
+		DrawBtn associationBtn = new DrawBtn(associationIcon, new Mode());
+		DrawBtn generalizationBtn = new DrawBtn(generalizationIcon, new Mode());
+		DrawBtn compositionBtn = new DrawBtn(compositionIcon, new Mode());
+		DrawBtn classBtn = new DrawBtn(classIcon, new Mode());
+		DrawBtn useCaseBtn = new DrawBtn(useCaseIcon, new Mode());
 		
 		// Add button into toolBar
 		this.add(selectBtn);
@@ -39,7 +50,35 @@ public class ToolBar extends JToolBar{
 		this.add(compositionBtn);
 		this.add(classBtn);
 		this.add(useCaseBtn);
-		
 	
+	}
+	
+	private class DrawBtn extends JButton {
+		Mode mode;
+		DrawBtn(ImageIcon icon,Mode mode) {
+			this.setIcon(icon);
+			this.mode = mode;
+			this.addActionListener(new BtnListener());
+		}
+		
+		private class BtnListener implements ActionListener {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("trigger");
+				
+				// Set button color
+				if(holdBtn != null) {
+					holdBtn.setBackground(Color.white);
+				}
+				holdBtn = (DrawBtn) e.getSource();
+				holdBtn.setBackground(Color.gray);
+				
+				// Set drawingArea mode
+				DrawingArea.getInstance().setMode(mode);
+			}
+			
+		}
+		
 	}
 }
